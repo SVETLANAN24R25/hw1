5860530052919163e689871437d09123c81fa547
SVETLANAN24R25 <136961364+SVETLANAN24R25@users.noreply.github.com>
2023-06-26 18:30:20 +0700

    Update дз.py

--- дз.py
+++ дз.py
@@ -1,9 +1,10 @@
 def pal(s): 
     return s[::-1] == s 
-
+#сравниваем конец и начало строки
 while True: 
     s = input("введите слово: ") 
     if pal(s):
         print('True')
     else:
         print('False')
+#если слово палиндром выводим 'True',если нет- 'False'
