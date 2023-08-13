7ccd74dd8fe3a88153c52d77a78e02736d551197
SVETLANAN24R25 <136961364+SVETLANAN24R25@users.noreply.github.com>
2023-08-13 23:22:57 +0700

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from .views import index,top_sellers,adv_post,reg,log,pro
 
-from . import views
-
-urlpatterns = [
-    path('lesson_4/', views.index),
+urlpatterns=[
+    path( '', index, name='main-page'),
+    path('top-sellers/',top_sellers,name='top-sellers'),
+    path('adv-post/',adv_post,name='adv-post'),
+    path('reg/',reg, name='reg'),
+    path('log/',log,name='log'),
+    path('pro/',pro,name='pro'),
 ]
--- views.py
+++ views.py
@@ -2,5 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Домашка по 4 занятию")
+    return render(request,'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+def adv_post(request):
+    return render(request,'advertisment-post.html')
+def reg(request):
+    return render(request,'register.html')
+    
+def log(request):
+    return render(request,'login.html')
+
+def pro(request):
+    return render(request,'profile.html')
+
+
+
+
+
+
 # Create your views here.
