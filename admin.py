1bed9d1467668aebbb538adbe87bddd646fbeb30
SVETLANAN24R25 <136961364+SVETLANAN24R25@users.noreply.github.com>
2023-08-28 12:25:41 +0700

    Add files via upload

--- admin.py
+++ admin.py
@@ -2,18 +2,23 @@ from django.contrib import admin
 from .models import Advertisment
 
 class AdvAdmin(admin.ModelAdmin):
-    list_display=['id','title','description','price','auction','created_date','updated_time']
+    list_display=['id','title','description','price','auction','created_date','updated_date','get_image_html']
     list_filter=['id','created_id']
     
 
-    actions=['make_auction_as_false']
+    actions=['make_auction_as_false','make_auction_as_true']
+
     @admin.action(description='Убрать возможность торга')
     def make_auction_as_false(self,request,queryset):
         queryset.update(auction=False)
 
+    @admin.action(description='Добавить возможность торга')
+    def make_auction_as_true(self,request,queryset):
+        queryset.update(auction=True)
+
     fieldsets=(
         ('Общее',{
-            'fields':('title','description'),
+            'fields':('title','description','image'),
         }),
         ('Финансы',{
             'fields': ('price','auction'),
--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+User=get_user_model()
 
 class Advertisment(models.Model):
     title= models.CharField('заголовок',max_length=128)
@@ -9,6 +12,8 @@ class Advertisment(models.Model):
     auction= models.BooleanField('торг',help_text='Отметьте , если торг уместен')
     created_id=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+    user=models.ForeignKey(User,verbose_name='пользователь',on_delete=models.CASCADE)
+    image=models.ImageField('изображение',upload_to='adv_65/')
 
 
     @admin.display(description='дата создания')
@@ -30,6 +35,12 @@ class Advertisment(models.Model):
                 '<span style=" color:blue; font-weight:bold;">Сегодня в {}</span>',updated_time
             )
         return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
+    @admin.display(description='фото')
+    def get_image_html(self):
+        if self.image:
+            return format_html(
+                '<img src="{url}" style="max-width: 80 px; max-height: 80 px;" ',url=self.get_image_html()
+            )
     
 
 
