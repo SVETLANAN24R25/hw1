7eed64c00172bd314475b7c4659be50ecbbc48fa
SVETLANAN24R25 <136961364+SVETLANAN24R25@users.noreply.github.com>
2023-08-29 20:53:55 +0700

    Add files via upload

--- views.py
+++ views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from .models import Advertisment
+from .forms import AdvertismentForm
 
 def index(request):
-    return render(request,'index.html')
+    advert=Advertisment.objects.all()
+    context={'advertisements':advert}
+    return render(request,'index.html',context)
 
 def top_sellers(request):
     return render(request, 'top-sellers.html')
 
 def adv_post(request):
-    return render(request,'advertisment-post.html')
+    if request.method=='POST':
+        form=AdvertismentForm(request.POST,request.FILES)
+        if form.is_valid():
+            new_adv=form.save(commit=False)
+            new_adv.user=request.user
+            new_adv.save()
+            url=reverse('main-page')
+            return redirect(url)
+    else:
+        form=AdvertismentForm()
+
+    context={'form':form}
+    return render(request,'advertisment-post.html',context)
 def reg(request):
     return render(request,'register.html')
     
@@ -17,6 +33,10 @@ def log(request):
 
 def pro(request):
     return render(request,'profile.html')
+
+
+    
+
 
 
 
